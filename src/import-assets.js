069c5c994ab07694742512a9ab2ebe306d6bfc20
Ian Band <ian.r.band@gmail.com>
2020-04-10 14:33:02 -0700

    added wildcard source as reference

--- src/import-assets.js
+++ src/import-assets.js
@@ -27,8 +27,45 @@ module.exports = function ImportAssets({types: t}) {
                     state.opts.configFilePath, 
                     "assetEntryPoints:\n",
                     entryPoints);
-                    //"entry points array:",
-                    //entryPoints);
+
+                let extensions = [...state.opts.extensions,
+                                    "png", "jpg", "jpeg", "svg", "", "json", "gif", "mp3"];
+
+                // get import object identifier
+                // import myAssets from "{assets}" => myAssets
+                console.log(path.node.specifiers[0].local.name);
+                let assetsIdentifier = path.node.specifiers[0].local.name;
+
+                // create asset object. 'import myAssets from "{assets}";'
+                // this creates 'const myAssets = {};'
+                let obj = t.variableDeclaration("const", [
+                    t.variableDeclarator(
+                        t.identifier(assetsIdentifier),
+                        t.objectExpression([])
+                    )
+                ]);
+                path.insertBefore(obj);
+
+
+                //for each entry point, 
+
+                // Get current filename so we can try to determine the folder
+                var name = state.file.opts.filename;
+
+                var files = [];
+                var isDirs = [];
+                var dir = _path.join(_path.dirname(name), src); // path of the target dir.
+
+
+
+
+
+
+
+
+                // remove original import statement 
+                // import myAssets from "{assets}"
+                path.remove();
             }
         }
     };
